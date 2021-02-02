6e93203ed455aafa3e01e3605030abfcd27db990
Joel Nieto <joenieto90@hotmail.com>
2021-02-02 12:22:36 -0600

    Componente Main.jsx; vista del detalle del prducto

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Cards from "./components/Cards";
+import Main from "./components/Main";
 import "./styles/App.scss";
 
 export default class App extends Component {
@@ -12,6 +13,7 @@ export default class App extends Component {
         <NavBar />
           <Switch>
             <Route exact path="/" component={Cards} />
+            <Route exact path="/main" component={Main} />
           </Switch>
         </div>
       </BrowserRouter>
---
+++ src/components/Main.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export default class Main extends Component {
+
+  render() {
+    return (
+      <section className="Main">
+        <div className="Main__Pictures">
+              <img
+                className="Main__Pictures--img"
+                src=''
+                alt='imagen'
+              />
+            )
+        </div>
+        <div className="Main__Image">
+          <img className="Main__Image--img" src='' />
+        </div>
+        <div className="Main__Info">
+          <small className="Main__Info--small">Nuevo | 6927 vendidos</small>
+          <p className="Main__Info--title">Titulo</p>
+          <p className="Main__Info--price">$Precio</p>
+          <a href='' className="Main__Info--button">
+            Comprar ahora
+          </a>
+          <ul className="Main__Info--list">
+                <li className="Main__Info--list--li">
+                  Atributo : Valor
+                </li>
+              )
+          </ul>
+        </div>
+      </section>
+    );
+  }
+}
